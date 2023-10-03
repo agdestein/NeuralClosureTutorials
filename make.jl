b707e40509fc6d2c41b127c49bf503e559c5c5ae
Syver Døving Agdestein <syverda@gmail.com>
2023-10-03 14:10:56 +0200

    Remove comment

--- make.jl
+++ make.jl
@@ -10,12 +10,3 @@ for f in files
     Literate.notebook(f, output_dir; execute = false)
     Literate.markdown(f, output_dir; codefence = "```julia" => "```")
 end
-
-# filename = "navier_stokes_spectral.jl"
-# filename = "burgers.jl"
-#
-# while true
-#     # Regenerate markdown file at every change
-#     watch_file(filename)
-#     Literate.markdown(filename; codefence = "```julia" => "```")
-# end
