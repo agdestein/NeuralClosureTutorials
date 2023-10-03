253ac26530785a683019064d75f39e27212b63b5
Syver Døving Agdestein <syverda@gmail.com>
2023-10-03 12:00:59 +0200

    Add action for creating markdown and notebook

--- make.jl
+++ make.jl
@@ -1,13 +1,21 @@
 using Literate
 using FileWatching
 
-filename = "navier_stokes_spectral.jl"
-filename = "burgers.jl"
+files = ["burgers.jl", "navier_stokes_spectral.jl"]
+output_dir = "generated"
 
-Literate.notebook(filename; execute = false)
+ispath(output_dir) || mkpath(output_dir)
 
-while true
-    # Regenerate markdown file at every change
-    watch_file(filename)
-    Literate.markdown(filename; codefence = "```julia" => "```")
+for f in files
+    Literate.notebook(f, output_dir; execute = false)
+    Literate.markdown(f, output_dir; codefence = "```julia" => "```")
 end
+
+# filename = "navier_stokes_spectral.jl"
+# filename = "burgers.jl"
+#
+# while true
+#     # Regenerate markdown file at every change
+#     watch_file(filename)
+#     Literate.markdown(filename; codefence = "```julia" => "```")
+# end
